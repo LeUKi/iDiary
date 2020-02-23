1a78e5c11f20ba482635fb76a13374c0d7834d11
Lafish <34953046+LeUKi@users.noreply.github.com>
2020-02-23 19:05:26 +0800

    js

--- js.js
+++ js.js
@@ -2,18 +2,15 @@
 if (localStorage.diaryData == undefined || localStorage.diaryData == "[null]") {
     localStorage.diaryData = "[]"
 }
-
 var dAtaStr = localStorage.diaryData;
 var dAtaObj = JSON.parse(dAtaStr);
 var focDiary = "";
-
 function showEdit(i) {//显示编辑
     $("#E1dit").val(dAtaObj[i].title);
     $("#E2dit").val(dAtaObj[i].diary);
     focDiary = i;
     focTime = dAtaObj[i].time;
 }
-
 function finEdit() {
     if ($("#E1dit").val()) {
         var s = '{"title":"' + $("#E1dit").val() + '","diary":"' + $("#E2dit").val() + '","time":"' + focTime + '"}';
@@ -21,12 +18,10 @@ function finEdit() {
         var o = JSON.parse(s);
         localStorage.tips = 2;
         chDiary(focDiary, 1, o);
-    } else (
+    } else {
         alert("没有标题的日记不作数哦！")
-    )
+    }
 }
-
-
 function getTime() {//时间格式化
     tIme = new Date();
     var year = tIme.getFullYear();
@@ -37,16 +32,13 @@ function getTime() {//时间格式化
     var sec = tIme.getSeconds();
     return year + "-" + mon + "-" + day + " " + hour + ":" + min + ":" + sec;
 }
-
 function chColor(className, cOlor1) {//对象切换颜色
     var oBj = document.getElementsByClassName(className);
     for (var i = 0; i < oBj.length; i++) {
         oBj[i].style.background = cOlor1;
 
     }
-
 }
-
 function chSkin() {//切换皮肤
     if (localStorage.skin == "1") {
         chSkin2();
@@ -54,21 +46,18 @@ function chSkin() {//切换皮肤
         chSkin1();
     }
 }
-
 function chSkin1() {//显示皮肤1
     localStorage.skin = 1;
     document.body.style.background = "#e2e2e2";//1
     chColor("chCol", "#ffffff",);
     chColor("card", "#ffffff");
 }
-
 function chSkin2() {//显示皮肤2
     localStorage.skin = 2;
     document.body.style.background = "#494949";//2
     chColor("chCol", "#e5e5e5");
     chColor("card", "#bababa");
 }
-
 function sAve() {
     if ($("#tiTle").val()) {
         var s = '{"title":"' + $("#tiTle").val() + '","diary":"' + $("#coNtent").val() + '","time":"' + getTime() + '"}';
@@ -80,12 +69,10 @@ function sAve() {
         alert("没有标题的日记不作数哦！")
     );
 }
-
 function unDo() {
 localStorage.diaryData=localStorage.undo;
 location.reload();
 }
-
 function chDiary(a, b, o) {
     if (o == null) {
         dAtaObj.splice(a, b);
@@ -96,13 +83,9 @@ function chDiary(a, b, o) {
 
     location.reload();
 }
-
 window.onload = function () {
     if (typeof (Storage) !== "undefined") {
-
         if (dAtaObj.length) {//非第一次
-
-
             var disDiary = "";
             for (var i = 0; i < dAtaObj.length; i++) {
                 disDiary = disDiary +
@@ -114,9 +97,7 @@ window.onload = function () {
                     "<button class=\"btn btn-sm btn-danger\" onclick=\'localStorage.tips = 3;localStorage.undo=dAtaStr;chDiary(" + i + ",1,);\' style=\"float: right\">删除</button><small>" + dAtaObj[i].time + "</small></div></div><br>";
             }
             document.getElementById("diAry").innerHTML = disDiary;
-
         } else {//第一次进入
-
             document.getElementById("diAry").innerHTML =
                 "<div class=\"card chCol\"><div class=\"card-header\">" +
                 "<button class=\"btn btn-sm btn-info\" style=\"float: right\" disabled>不可编辑</button>" +
@@ -127,7 +108,6 @@ window.onload = function () {
         }//进入后
         if (localStorage.skin == 2) {
             chSkin2();
-
         } else {
             chSkin1();
             localStorage.skin = 1;//初始化皮肤
@@ -153,11 +133,7 @@ window.onload = function () {
                 "</div>");
             localStorage.tips = 0;
         }
-
-
     } else {//无法进入
         alert("你的浏览器不支持");
     }
-
-
 };
